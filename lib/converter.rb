418be45a57f6fc293b96743d2f08bf52c363d9c4
S-Spiegl <steven.spiegl@gmail.com>
2022-06-11 13:08:53 +0100

    converted to class to store data

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,21 +1,27 @@
-def convert(number)
-  if number == 0 
-    return ""
-  elsif number.between?(1, 3)
-    return "I" * number
-  elsif number == 4
-    return "IV"
-  elsif number == 5
-    return "V"
-  elsif number == 6
-    return "VI"
-  elsif number == 7
-    return "VII"
-  elsif number == 8
-    return "VIII"
-  elsif number == 9
-    return "IX"
-  elsif number == 10
-    return "X"
+class RomanNumerals
+  def convert(number)
+    if number == 0 
+      return ""
+    elsif number.between?(1, 3)
+      return "I" * number
+    elsif number == 4
+      return "IV"
+    elsif number == 5
+      return "V"
+    elsif number == 6
+      return "VI"
+    elsif number == 7
+      return "VII"
+    elsif number == 8
+      return "VIII"
+    elsif number == 9
+      return "IX"
+    elsif number == 10
+      return "X"
+    end
   end
 end
+
+# elsif number.between?(6, 8)
+#   string.push "V" 
+#   (number-5).times{string.push "I"}
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,51 +1,50 @@
 require './lib/converter'
+describe RomanNumerals do
+  describe '#convert' do
+    it 'should convert 0 to an empty string' do
+      expect(subject.convert(0)).to eq('')
+    end
 
-describe '#convert' do
-  it 'should convert 0 to an empty string' do
-    expect(convert(0)).to eq('')
+    it 'should convert 1 to I' do
+      expect(subject.convert(1)).to eq("I")
+    end
+
+    it 'should convert 2 to II' do
+      expect(subject.convert(2)).to eq("II")
+    end
+
+    it 'should convert 3 to III' do
+      expect(subject.convert(3)).to eq("III")
+    end 
+
+    it 'should convert 4 to IV' do
+      expect(subject.convert(4)).to eq("IV")
+    end
+
+    it 'should convert 5 to V' do
+      expect(subject.convert(5)).to eq("V")
+    end 
+
+    it 'should convert 6 to VI' do
+      expect(subject.convert(6)).to eq("VI")
+    end 
+    
+    it 'should convert 7 to VII' do
+      expect(subject.convert(7)).to eq("VII")
+    end 
+
+    it 'should convert 8 to VIII' do
+      expect(subject.convert(8)).to eq("VIII")
+    end 
+    
+    it 'should convert 9 to IX' do
+      expect(subject.convert(9)).to eq("IX")
+    end 
+
+    it 'should convert 10 to X' do
+      expect(subject.convert(10)).to eq("X")
+    end 
   end
-
-  it 'should convert 1 to I' do
-    expect(convert(1)).to eq("I")
-  end
-
-  it 'should convert 2 to II' do
-    expect(convert(2)).to eq("II")
-  end
-
-  it 'should convert 3 to III' do
-    expect(convert(3)).to eq("III")
-  end 
-
-  it 'should convert 4 to IV' do
-    expect(convert(4)).to eq("IV")
-  end
-
-  it 'should convert 5 to V' do
-    expect(convert(5)).to eq("V")
-  end 
-
-  it 'should convert 6 to VI' do
-    expect(convert(6)).to eq("VI")
-  end 
-  
-  it 'should convert 7 to VII' do
-    expect(convert(7)).to eq("VII")
-  end 
-
-  it 'should convert 8 to VIII' do
-    expect(convert(8)).to eq("VIII")
-  end 
-  
-  it 'should convert 9 to IX' do
-    expect(convert(9)).to eq("IX")
-  end 
-
-  it 'should convert 10 to X' do
-    expect(convert(10)).to eq("X")
-  end 
-
-
 end
 
 
